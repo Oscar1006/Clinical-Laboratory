433a1e15b61928809f31256530a9d2c31795bcfa
Oscar <oscar.osv10@gmail.com>
2022-10-07 14:02:15 -0500

    Pile structure unit testing complete

--- ClinicLabFX/src/test/java/data_strutures/PileTest.java
+++ ClinicLabFX/src/test/java/data_strutures/PileTest.java
@@ -12,6 +12,7 @@ import exception.PileException;
 public class PileTest {
 	
 	private Pile<String> pile;
+	private Pile<Object> pile2;
 	
 	public void setUpStage1() {
 		pile = new Pile<>();
@@ -26,6 +27,16 @@ public class PileTest {
 		pile = new Pile<>();
 		pile.push("Hello");
 		pile.pop();
+		
+	}
+	public void setUpStage4() throws PileException {
+		pile = new Pile<>();
+		pile.push("Hello");
+		pile.push("World");
+	}
+	
+	private void setUpStage5() {
+		pile = new Pile<String>();
 		
 	}
 	
@@ -66,7 +77,7 @@ public class PileTest {
 			pile.pop();
 			fail("Should throw exception");
 		} catch (PileException e) {
-			System.out.println(e.getMessage());
+			System.out.println("Nice: " + e.getMessage());
 		}
 		assertTrue(pile.isEmpty());
 	}
@@ -79,7 +90,7 @@ public class PileTest {
 			expected = pile.pop();
 		} catch (PileException e) {
 			System.out.println(e.getMessage());
-			fail("Should throw exception");
+			fail("Should not throw exception");
 		}
 		assertTrue(pile.isEmpty());
 		assertSame("Hello", expected.getInfo());
@@ -89,14 +100,110 @@ public class PileTest {
 	public void testPop3() {
 		Element<String> expected = new Element<>();
 		try {
-			setUpStage2();
+			setUpStage4();
 			expected = pile.pop();
 		} catch (PileException e) {
 			System.out.println(e.getMessage());
-			fail("Should throw exception");
+			fail("Should not throw exception");
 		}
-		assertTrue(pile.isEmpty());
-		assertSame("Hello", expected.getInfo());
+		try {
+			assertSame("Hello", pile.pop().getInfo());
+		} catch (PileException e) {
+			e.printStackTrace();
+		}
+		assertSame("World", expected.getInfo());
+		assertSame("Hello", expected.getNext().getInfo());
 	}
 
+	@Test
+	public void testPush() {
+		setUpStage1();
+	
+		try {
+			pile.push("Nice job");
+		} catch (PileException e) {
+			System.out.println( e.getMessage());
+			fail("Should not throw exception");
+		}
+		assertTrue(!pile.isEmpty());
+		try {
+			assertEquals("Wrong", "Nice job", pile.top().getInfo());
+		} catch (PileException e) {
+			fail("Should not throw exception");
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testPush2() {
+		setUpStage5();
+	
+		try {
+			pile2.push(1);
+			pile2.push("One");
+			fail("Should  throw exception");
+		} catch (Exception e) {
+			System.out.println( e.getMessage());
+		}
+		assertTrue(pile.isEmpty());
+		
+	}
+	
+	@Test
+	public void testPush3() {
+		setUpStage5();
+	
+		try {
+			pile2.push(1);
+			pile2.push("One");
+			fail("Should  throw exception");
+		} catch (Exception e) {
+			System.out.println("Nice: " + e.getMessage());
+		}
+		assertTrue(pile.isEmpty());
+		
+	}
+	
+	@Test
+	public void testTop() {
+		setUpStage1();
+	
+		try {
+			pile.top();
+			fail("Should  throw exception");
+		} catch (Exception e) {
+			System.out.println("Nice: " + e.getMessage());
+		}
+		assertTrue(pile.isEmpty());
+	}
+	
+	@Test
+	public void testTop2() {
+		setUpStage1();
+		String expected = "";
+		try {
+			pile.push("Hello");
+			expected= pile.top().getInfo();
+		} catch (Exception e) {
+			fail("Should  throw exception");
+			System.out.println(e.getMessage());
+		}
+		assertEquals("Hello", expected);
+	}
+
+	@Test
+	public void testTop3() {
+		try {
+			setUpStage2();
+			pile.pop();
+			pile.top();
+			fail("Should throw exception");
+		} catch (PileException pe) {
+			System.out.println("Nice: " + pe.getMessage());
+			
+		}
+		assertTrue(pile.isEmpty());
+		
+	}
+	
 }
